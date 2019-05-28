3fc0a8b94e920daae0c7dcce13c8182613f1f394
lalit <lalitpatil80@gmail.com>
2019-05-28 22:56:49 +0530

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent {
   uName="";
   dob="15/12/2019";
   companyName="Aarya.ai";
+  modifyTxt=false;
 
 onSubmit(){
   this.userName=this.uName;
@@ -24,6 +25,7 @@ onSubmit(){
 fnLogOut(){
   this.isLogged=true;
 }
+
 post=[
   {"postNo":"1","postTxt":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."},
   {"postNo":"2","postTxt":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."},
